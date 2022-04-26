3ba0e7b10181515df10e264c54cada2ba12aa93f
JonCabot <jfc0053@gmail.com>
2022-04-25 21:34:21 -0500

    models & migrations created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,27 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get '/events' do
+    events = Event.all
+    events.to_json
+  end
+
+  post '/events/:id' do
+    event = Event.create(
+      event_name: params[:event_name],
+      amount: params[:amount],
+      event_id: params[:event_id]
+    )
+    event.to_json
+  end
+
+  patch '/events/:id' do
+    event = Event.find(params[:id])
+    event.update(
+      event_name: params[:event_name],
+      amount: params[:amount]
+    )
+    event.to_json
   end
 
 end
